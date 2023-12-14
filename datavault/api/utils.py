b50580a72f916a1be7a2a043af21bf3f40550833
chahat sagar <chahatsagar2003@gmail.com>
2023-12-14 20:42:11 +0530

    Setup encryption and decryption function

--- datavault/api/utils.py
+++ datavault/api/utils.py
@@ -8,9 +8,18 @@ def encrypt_password(password):
     load_dotenv()
     
     key = os.getenv('ENCRYPTION_KEY')
+    fernet = Fernet(key)
+    encrypted_password = fernet.encrypt(password.encode())
+    return encrypted_password
     
-    print(key)
+def decrypt_password(encrypted_password):
     
+    load_dotenv()
+    key = os.getenv('ENCRYPTION_KEY')
+    fernet = Fernet(key)
+    decrypted_password = fernet.decrypt(encrypted_password)
+    print(decrypted_password)
+    return decrypted_password.decode()
 
 def generate_key():
     key = Fernet.generate_key()
@@ -26,4 +35,5 @@ def function():
 if __name__ == "__main__":
     # generate_key()
     # function()
-    
+    ans = decrypt_password(encrypt_password("aaa"))
+    print(ans)
